4fa8ee789bb57ccb30056f1adae1a61db78fd750
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-10 12:16:27 -0500

    🦖 8. restockCake:
    In redux the convention is to use a property called 'payload' for any
    additional information you want to send.

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -7,7 +7,7 @@ const orderCake = () => {
   return (
     {
       type: CAKE_ORDERED,
-      quantity: 1,
+      payload: 1,
     }
   )
 }
@@ -16,7 +16,7 @@ const restockCake = (qty = 1) => {
   return (
     {
       type: CAKE_RESTOCKED,
-      quantity: qty,
+      payload: qty,
     }
   )
 }
@@ -39,7 +39,7 @@ const reducer = (state = initialState, action) => {
     case CAKE_RESTOCKED:
       return {
         ...state,
-        numOfCakes: state.numOfCakes + action.quantity,
+        numOfCakes: state.numOfCakes + action.payload,
       }
     default:
       return state;
@@ -54,7 +54,7 @@ const unsubscribe = store.subscribe(() => {
 })
 
 store.dispatch(orderCake()); // remove 1
-store.dispatch({ type: CAKE_ORDERED, quantity: 1 });
+store.dispatch({ type: CAKE_ORDERED, payload: 1 });
 store.dispatch(orderCake()); // remove 1
 store.dispatch(restockCake(3)); // add 3 to have qty back to 10
 
