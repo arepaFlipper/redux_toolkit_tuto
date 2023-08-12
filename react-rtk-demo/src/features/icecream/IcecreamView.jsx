683e36c4fb96de8a3fd1e22f2c5c746a4aa91b22
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-12 09:10:28 -0500

    🎏 28 useDispatch

--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -6,12 +6,12 @@ export const IcecreamView = () => {
   const [value, setValue] = useState(1);
   const numberOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   const dispatch = useDispatch();
-  const restockValue = 3;
   return (
     <div>
       <h2>Number of ice creams - {numberOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
-      <button onClick={() => dispatch(restocked(restockValue))}>Restock {restockValue} Ice creams</button>
+      <input type="number" value={value} onChange={e => setValue(parseInt(e.target.value))} />
+      <button onClick={() => dispatch(restocked(value))}>Restock {value} Ice creams</button>
     </div>
   )
 }
