e76d3267aabd736952959214ed480f9fbcb40f1b
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-11 22:37:52 -0500

    🍢 27 useSelector typeError

--- react-rtk-demo/src/features/cake/Cake_view.jsx
+++ react-rtk-demo/src/features/cake/Cake_view.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Cake_view = () => {
+  const num_of_cakes = useSelector((state) => state.cake.numOfCakes);
   return (
     <div>
-      <h2>Number of Cakes - </h2>
+      <h2>Number of Cakes -{num_of_cakes} </h2>
       <button>Order Cake</button>
       <button>Restore Cakes</button>
     </div>
