84accaa088ea4e12126739a8001845c437b1e614
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-08-12 09:08:09 -0500

    ⛅ restock value display

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -5,11 +5,12 @@ import { ordered, restocked } from './cakeSlice';
 export const CakeView = () => {
   const numberOfCakes = useSelector((state) => state.cake.numOfCakes)
   const dispatch = useDispatch()
+  const restockValue = 5
   return (
     <div>
       <h2>Number of cakes - {numberOfCakes} </h2>
       <button onClick={() => dispatch(ordered())}>Order Cake</button>
-      <button onClick={() => dispatch(restocked(5))}>Restock Cakes</button>
+      <button onClick={() => dispatch(restocked(restockValue))}>Restock {restockValue} Cakes</button>
     </div>
   )
 }
--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -6,11 +6,12 @@ export const IcecreamView = () => {
   const [value, setValue] = useState(1);
   const numberOfIcecreams = useSelector((state) => state.icecream.numOfIcecreams);
   const dispatch = useDispatch();
+  const restockValue = 3;
   return (
     <div>
       <h2>Number of ice creams - {numberOfIcecreams}</h2>
       <button onClick={() => dispatch(ordered())}>Order Ice cream</button>
-      <button onClick={() => dispatch(restocked(3))}>Restock Ice creams</button>
+      <button onClick={() => dispatch(restocked(restockValue))}>Restock {restockValue} Ice creams</button>
     </div>
   )
 }
